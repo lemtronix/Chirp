a8451a3688d0a07f26e573e9751759d26f64a162
lemtronix <lembergerm@yahoo.com>
2016-08-14 13:34:43 -0500

    Added GPLv3 license to code

--- Amplifier.cpp
+++ Amplifier.cpp
@@ -1,3 +1,20 @@
+/*
+    This file is part of Chirp.
+
+    Chirp is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    Chirp is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Chirp.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 /** Power up sequence
  *  Notes:
  *  -On power up, the TCON register is set to 0xFF.  This is responsible for "connecting" the pots.  In Chirp, We use P0 as a rheostat and P1 as a potentiometer (disconnect P0B <b0>)
--- Amplifier.h
+++ Amplifier.h
@@ -1,3 +1,20 @@
+/*
+    This file is part of Chirp.
+
+    Chirp is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    Chirp is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Chirp.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 /** @todo Change this class to abstract out the fact that Chirp uses an pot to control the output voltage, perhaps AmplifierClass?
 */
 
